5ef372ed42c9c6dd8683c31b63f66011463136bf
spyderpz2 <hhmeijden@gmail.com>
2020-12-15 13:41:21 +0100

    cleaned up form1.cs and removed the new game lines into a seperate function

--- Form1.cs
+++ Form1.cs
@@ -18,7 +18,7 @@ namespace Reversi
         public String playerTwo;
         public int boardSize = 6;
         TableLayoutPanel board = new TableLayoutPanel();
-        public int[,] boardState = new int[6, 6];
+        public int[,] boardState;
         int currentPlayer = 1;
         IDictionary<int, Color> pieceColor = new Dictionary<int, Color> { { 1, Color.Black }, { 2, Color.White } };
         
@@ -42,38 +42,10 @@ namespace Reversi
 
             this.Text = "Reversi";
 
-            int columns = 6;
-            int rows = 6;
 
             this.Width = 800;
             this.Height = 800;
 
-            this.board.Name = "board";
-            this.board.Location = new Point(200, 200);
-            this.board.Size = new Size(50 * boardSize + boardSize, 50 * boardSize + boardSize);
-            this.board.BackColor = Color.DarkGreen;
-
-            this.board.CellPaint += new TableLayoutCellPaintEventHandler(this.draw);
-
-            for (; this.board.ColumnCount < columns; this.board.ColumnCount++)
-            {
-                this.board.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            }
-            for (; this.board.RowCount < rows; this.board.RowCount++)
-            {
-                this.board.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
-            }
-            this.board.MouseClick += new MouseEventHandler(this.Clicked);
-
-            //Setup first 4 pieces in the middle of the board:
-            boardState[columns / 2 - 1, rows / 2 - 1] = 1;
-            boardState[columns / 2 - 1, rows / 2] = 2;
-            boardState[columns / 2, rows / 2] = 1;
-            boardState[columns / 2, rows / 2 - 1] = 2;
-
-            this.board.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
-            // this is the on click function for each panel in the tablelayoutpanel, it does this by adding a control to each panel
-            Controls.Add(this.board);
             
         }
 
@@ -243,7 +215,7 @@ namespace Reversi
                     this.playerTwo = newGame.namePlayerTwo;
                     this.boardSize = newGame.gameBoardSize;
                     Console.WriteLine("New \"" + this.playerOne + "\"v\"" + this.playerTwo + "\" game");
-                    this.NewGame();
+                    this.NewGame(this.boardSize, this.boardSize);
                 }
                 else
                 {
@@ -263,7 +235,7 @@ namespace Reversi
                     this.playerTwo = newGame.namePlayerTwo;
                     this.boardSize = newGame.gameBoardSize;
                     Console.WriteLine("New \"" + this.playerOne + "\"v\"" + this.playerTwo + "\" game");
-                    this.NewGame();
+                    this.NewGame(this.boardSize, this.boardSize);
                 }
                 else
                 {
@@ -294,161 +266,46 @@ namespace Reversi
 
         }
 
-        private void NewGame()
+        private void NewGame(int columns, int rows) 
         {
             //TODO: Initialise gameBoard with new values.
             this.playerOneIconName.Text = this.playerOne;
             this.playerTwoIconName.Text = this.playerTwo;
+            boardState = new int[columns, rows];
+            this.board.Name = "board";
+            this.board.Location = new Point(200, 200);
+            this.board.Size = new Size(50 * columns + columns, 50 * rows + rows);
+            this.board.BackColor = Color.DarkGreen;
+            this.board.CellPaint += new TableLayoutCellPaintEventHandler(this.draw);
+
+            for (; this.board.ColumnCount < columns; this.board.ColumnCount++)
+            {
+                this.board.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            }
+            for (; this.board.RowCount < rows; this.board.RowCount++)
+            {
+                this.board.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            }
+            this.board.MouseClick += new MouseEventHandler(this.Clicked);
+
+
+
+            this.board.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+
+            Controls.Add(this.board);
+
+
+            //Setup first 4 pieces in the middle of the board:
+            boardState[columns / 2 - 1, rows / 2 - 1] = 1;
+            boardState[columns / 2 - 1, rows / 2] = 2;
+            boardState[columns / 2, rows / 2] = 1;
+            boardState[columns / 2, rows / 2 - 1] = 2;
         }
 
     }
 
 
-    public partial class PopupWindow : Form
-    {
-        public bool isPerson;
-        private TextBox playerOneTextBox;
-        private TextBox playerTwoTextBox;
-        private TextBox gameBoardTextBox;
 
-        public String namePlayerOne;
-        public String namePlayerTwo;
-        public int gameBoardSize;
-
-        public PopupWindow(bool isPerson)
-        {
-            this.isPerson = isPerson;
-            this.ControlBox = false;
-            this.Size = new Size(250, 300);
-            this.Text = "Stel game in";
-
-            //Player one textbox
-            this.playerOneTextBox = new TextBox();
-            this.playerOneTextBox.Size = new Size(200, 100);
-            this.playerOneTextBox.Location = new Point(20, 40);
-            this.playerOneTextBox.Name = "playerOne";
-            this.Controls.Add(this.playerOneTextBox);
-
-            Label playerOneLabel = new Label();
-            playerOneLabel.Text = "Naam speler 1";
-            playerOneLabel.Location = new Point(20, 20);
-            playerOneLabel.Name = "labelPlayerOne";
-            this.Controls.Add(playerOneLabel);
-
-            //Player two textbox
-            if (isPerson)
-            {
-                Label playerTwoLabel = new Label();
-                playerTwoLabel.Text = "Naam speler 2";
-                playerTwoLabel.Location = new Point(20, 75);
-                playerTwoLabel.Name = "labelPlayerTwo";
-                this.Controls.Add(playerTwoLabel);
-
-                this.playerTwoTextBox = new TextBox();
-                this.playerTwoTextBox.Size = new Size(200, 100);
-                this.playerTwoTextBox.Location = new Point(20, 100);
-                this.playerTwoTextBox.Name = "playerTwo";
-                this.Controls.Add(this.playerTwoTextBox);
-            }
-
-            Label boardSizeLabel = new Label();
-            boardSizeLabel.Text = "Afmeting van het spelbord";
-            boardSizeLabel.Location = new Point(20, 130);
-            boardSizeLabel.Size = new Size(200, 20);
-            boardSizeLabel.Name = "labelBoardSize";
-            this.Controls.Add(boardSizeLabel);
-
-            this.gameBoardTextBox = new TextBox();
-            this.gameBoardTextBox.Size = new Size(50, 100);
-            this.gameBoardTextBox.Location = new Point(20, 150);
-            this.gameBoardTextBox.Name = "gameBoardTextBox";
-            this.Controls.Add(this.gameBoardTextBox);
-
-
-
-            //Cancel button
-            Button cancelButton = new Button();
-            cancelButton.Size = new Size(80, 50);
-            cancelButton.Location = new Point(20, 180);
-            cancelButton.Name = "cancelButton";
-            cancelButton.Text = "Annuleer";
-            cancelButton.Click += this.cancelClick;
-            this.CancelButton = cancelButton;
-
-            this.Controls.Add(cancelButton);
-
-            //Submit button
-            Button submitButton = new Button();
-            submitButton.Size = new Size(80, 50);
-            submitButton.Location = new Point(140, 180);
-            submitButton.Name = "submitButton";
-            submitButton.Text = "Start game";
-            submitButton.Click += this.submitClick;
-            this.AcceptButton = submitButton;
-
-            this.Controls.Add(submitButton);
-
-
-
-        }
-
-        private void submitClick(object o, EventArgs e)
-        {
-            bool allOk = true;
-            string playerOne = this.playerOneTextBox.Text;
-            string playerTwo = (this.isPerson ? this.playerTwoTextBox.Text : "Bot Frank");
-            int BoardSize;
-            if (int.TryParse(this.gameBoardTextBox.Text, out BoardSize))
-            {
-                //parsing successful 
-
-                if (BoardSize < 3 && BoardSize > 10)
-                {
-                    //Board heeft juiste afmetingen 
-                    allOk = false;
-                }
-            }
-            else
-            {
-                //parsing failed.
-                allOk = false;
-            }
-
-            if (String.IsNullOrEmpty(this.playerOneTextBox.Text))
-            {
-                allOk = false;
-            }
-
-            if (this.isPerson && String.IsNullOrEmpty(this.playerOneTextBox.Text))
-            {
-                allOk = false;
-            }
-
-
-            if (allOk)
-            {
-                this.namePlayerOne = playerOne;
-                this.namePlayerTwo = playerTwo;
-                this.gameBoardSize = BoardSize;
-                this.DialogResult = DialogResult.OK;
-                this.Close();
-            }
-            else
-            {
-                Console.WriteLine("Fix je inputs");
-            }
-
-
-        }
-
-        private void cancelClick(object o, EventArgs e)
-        {
-            this.DialogResult = DialogResult.Cancel;
-            this.Close();
-        }
-
-
-    }
 
 
 }
---
+++ PopupWindow.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public partial class PopupWindow : Form
+{
+    public bool isPerson;
+    private TextBox playerOneTextBox;
+    private TextBox playerTwoTextBox;
+    private TextBox gameBoardTextBox;
+
+    public String namePlayerOne;
+    public String namePlayerTwo;
+    public int gameBoardSize;
+
+    public PopupWindow(bool isPerson)
+    {
+        this.isPerson = isPerson;
+        this.ControlBox = false;
+        this.Size = new Size(250, 300);
+        this.Text = "Stel game in";
+
+        //Player one textbox
+        this.playerOneTextBox = new TextBox();
+        this.playerOneTextBox.Size = new Size(200, 100);
+        this.playerOneTextBox.Location = new Point(20, 40);
+        this.playerOneTextBox.Name = "playerOne";
+        this.Controls.Add(this.playerOneTextBox);
+
+        Label playerOneLabel = new Label();
+        playerOneLabel.Text = "Naam speler 1";
+        playerOneLabel.Location = new Point(20, 20);
+        playerOneLabel.Name = "labelPlayerOne";
+        this.Controls.Add(playerOneLabel);
+
+        //Player two textbox
+        if (isPerson)
+        {
+            Label playerTwoLabel = new Label();
+            playerTwoLabel.Text = "Naam speler 2";
+            playerTwoLabel.Location = new Point(20, 75);
+            playerTwoLabel.Name = "labelPlayerTwo";
+            this.Controls.Add(playerTwoLabel);
+
+            this.playerTwoTextBox = new TextBox();
+            this.playerTwoTextBox.Size = new Size(200, 100);
+            this.playerTwoTextBox.Location = new Point(20, 100);
+            this.playerTwoTextBox.Name = "playerTwo";
+            this.Controls.Add(this.playerTwoTextBox);
+        }
+        //boardsize
+        Label boardSizeLabel = new Label();
+        boardSizeLabel.Text = "Afmeting van het spelbord";
+        boardSizeLabel.Location = new Point(20, 130);
+        boardSizeLabel.Size = new Size(200, 20);
+        boardSizeLabel.Name = "labelBoardSize";
+        this.Controls.Add(boardSizeLabel);
+
+        this.gameBoardTextBox = new TextBox();
+        this.gameBoardTextBox.Size = new Size(50, 100);
+        this.gameBoardTextBox.Location = new Point(20, 150);
+        this.gameBoardTextBox.Name = "gameBoardTextBox";
+        this.Controls.Add(this.gameBoardTextBox);
+
+
+
+        //Cancel button
+        Button cancelButton = new Button();
+        cancelButton.Size = new Size(80, 50);
+        cancelButton.Location = new Point(20, 180);
+        cancelButton.Name = "cancelButton";
+        cancelButton.Text = "Annuleer";
+        cancelButton.Click += this.cancelClick;
+        this.CancelButton = cancelButton;
+
+        this.Controls.Add(cancelButton);
+
+        //Submit button
+        Button submitButton = new Button();
+        submitButton.Size = new Size(80, 50);
+        submitButton.Location = new Point(140, 180);
+        submitButton.Name = "submitButton";
+        submitButton.Text = "Start game";
+        submitButton.Click += this.submitClick;
+        this.AcceptButton = submitButton;
+
+        this.Controls.Add(submitButton);
+
+
+
+    }
+
+    private void submitClick(object o, EventArgs e)
+    {
+        bool allOk = true;
+        string playerOne = this.playerOneTextBox.Text;
+        string playerTwo = (this.isPerson ? this.playerTwoTextBox.Text : "Bot Frank");
+        int BoardSize;
+        if (int.TryParse(this.gameBoardTextBox.Text, out BoardSize))
+        {
+            //parsing successful 
+
+            if (BoardSize < 3 && BoardSize > 10)
+            {
+                //Board heeft juiste afmetingen 
+                allOk = false;
+            }
+        }
+        else
+        {
+            //parsing failed.
+            allOk = false;
+        }
+
+        if (String.IsNullOrEmpty(this.playerOneTextBox.Text))
+        {
+            allOk = false;
+        }
+
+        if (this.isPerson && String.IsNullOrEmpty(this.playerOneTextBox.Text))
+        {
+            allOk = false;
+        }
+
+
+        if (allOk)
+        {
+            this.namePlayerOne = playerOne;
+            this.namePlayerTwo = playerTwo;
+            this.gameBoardSize = BoardSize;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+        else
+        {
+            Console.WriteLine("Fix je inputs");
+        }
+
+
+    }
+
+    private void cancelClick(object o, EventArgs e)
+    {
+        this.DialogResult = DialogResult.Cancel;
+        this.Close();
+    }
+
+
+}
